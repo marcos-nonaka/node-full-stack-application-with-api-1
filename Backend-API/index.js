1c482d6d4c597626fbbd2964a98e1dbb4af9fa4e
marcos-nonaka <marcos.nonaka@gmail.com>
2023-06-10 17:02:32 -0300

    - Node.js Full Stack Application with API consume

---
+++ Backend-API/index.js
@@ -0,0 +1,25 @@
+//BIBLIOTECAS/MODULOS UTILIZADOS
+const database = require("./db/db");
+const routes = require("./routes/routes");
+const express = require("express");
+const app = express();
+
+//CODIFICAÇÃO UTILIZAÇÃO DO JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//MODELS
+const Usuario = require("./model/usuarioModel");
+const Tarefa = require("./model/tarefasModel");
+
+try {
+  database.sync().then(() => {
+  });
+} catch (erro) {
+  console.log("Houve uma falha ao sincronizar com o banco de dados. ", erro);
+};
+
+//ROTA PRINCIPAL
+app.use("/",routes);
+
+app.listen(3000);
---
+++ Frontend/routes/routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const Services = require("../services/services");
+const router = express.Router();
+
+router.get("/",(req, res) => {
+  res.send("Seja bem Vindo ao nosso sistema de Tarefas.");
+});
+
+router.get("/tarefas/cadastrar",(req, res) => {
+  res.render("tarefas/cadastrar");
+});
+
+router.get("/tarefas/Atualizar/:id_tarefa/:titulo/:descricao",(req, res) => {
+  let tarefas = {
+    id_tarefa : req.params.id_tarefa,
+    titulo : req.params.titulo,
+    descricao : req.params.descricao
+  };
+  res.render("tarefas/update",{tarefas});
+});
+
+//ROTA PARA SERVIÇO DE CREATE
+router.post("/tarefas/Create",Services.TarefaCreate);
+
+//ROTA PARA O SERVIÇO LISTAR
+router.get("/tarefas/listar",Services.TarefaListar);
+
+//ROTA PARA O SERVIÇO ALTERAR
+router.post("/tarefas/Update",Services.TarefaUpdate);
+
+//ROTA PARA O SERVIÇO DE DELETE
+router.post("/tarefas/Delete",Services.TarefaDelete);
+
+module.exports = router;
